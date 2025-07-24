fd782a67673d65c90664ad6a8d1bb52d5ec4d50c
bahareh-bahrami <bahareh.bahramy@gmail.com>
2025-07-24 15:59:28 +0330

    Add Display Movements Feature

--- script.js
+++ script.js
@@ -75,18 +75,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-for (const [item, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${item + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${item + 1}: You withdrawed ${Math.abs(movement)}`);
-  }
-}
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = ' ';
 
-movements.forEach(function (movement, item, arry) {
-  if (movement > 0) {
-    console.log(`Movement ${item + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${item + 1}: You withdrawed ${Math.abs(movement)}`);
-  }
-});
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+     <div class="movements__type movements__type--${type}"> ${
+      i + 1
+    } ${type}</div>
+     <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
